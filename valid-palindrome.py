6a2272cc7570b34b6b4da880b67797afb57b9267
Michael Blakesley <m.e.blakesley@gmail.com>
2022-08-24 22:29:49 -0700

    Add valid-palindrome

---
+++ valid-palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    # O(n)!
+    def isPalindrome(self, s: str) -> bool:
+        # for some reason, list + [::-1] is slow, but list + join() is fast
+        s_plain: str = ''.join([char.lower() for char in s if char.isalnum()])
+        # ideally we'd only check half the str
+        return s_plain == s_plain[::-1]
+
+
+print(Solution().isPalindrome('A man, a plan, a canal: Panama!'))
