3d34aa9e659bec0a87fed650d8598955762932c2
Mike Blakesley <m.e.blakesley@gmail.com>
2022-09-08 16:04:28 -0700

    Add grind75 #19: reverse linked list

---
+++ grind75-19-reverse-linked-list.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val: int = val
+        self.next: Optional[ListNode] = next
+
+
+# https://leetcode.com/problems/reverse-linked-list/
+class Solution:
+    # constructive version
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        rev_prev = None
+        while head:
+            rev_head = ListNode(head.val, rev_prev)
+            head = head.next
+            rev_prev = rev_head
+        return rev_prev
