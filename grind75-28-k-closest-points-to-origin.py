39dcdc2d5c040d762472454bfc1db5ba79c84c6d
Mike Blakesley <m.e.blakesley@gmail.com>
2022-09-18 16:25:25 -0700

    Add grind75 #28: k closest points to origin

---
+++ grind75-28-k-closest-points-to-origin.py
@@ -0,0 +1,22 @@
+import heapq
+
+
+# https://leetcode.com/problems/k-closest-points-to-origin/
+class Solution:
+    # O(n log k) using heap, 1-liner
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        return heapq.nsmallest(k, points, lambda xy: xy[0]**2 + xy[1]**2)
+
+    # O(n log k) using heap, done more manually to learn about heaps
+    def kClosest_heap_long(self, points: list[list[int]], k: int) -> list[list[int]]:
+        heap = [(-x**2 - y**2, x, y) for x, y in points[:k]]
+        heapq.heapify(heap)
+        for x, y in points[k:]:
+            sum_ = -x**2 - y**2
+            if sum_ > heap[0][0]:
+                heapq.heapreplace(heap, (sum_, x, y))
+        return [[x, y] for _, x, y in heap]
+
+    # O(k * n), more naive 1-liner, before I learned about heaps
+    def kClosest_kn(self, points: list[list[int]], k: int) -> list[list[int]]:
+        return sorted(points, key=lambda xy: xy[0]**2 + xy[1]**2)[:k]
