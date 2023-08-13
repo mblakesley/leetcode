ab3be20cc1342569105d1fd2753640bcae1ad740
Mike Blakesley <m.e.blakesley@gmail.com>
2023-08-13 15:11:29 -0700

    Update grind75 #5 in Python (valid-palindrome)

--- grind75-05-valid-palindrome.py
+++ grind75-05-valid-palindrome.py
@@ -2,10 +2,6 @@
 class Solution:
     # O(n)!
     def isPalindrome(self, s: str) -> bool:
-        # for some reason, list + [::-1] is slow, but list + join() is fast
+        # yay comprehensions, but also, string immutability means looped '+=' is inefficient
         s_plain: str = ''.join([char.lower() for char in s if char.isalnum()])
-        # ideally we'd only check half the str
         return s_plain == s_plain[::-1]
-
-
-print(Solution().isPalindrome('A man, a plan, a canal: Panama!'))
