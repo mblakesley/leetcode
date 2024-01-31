0e31720c5d928e47687a719e8cb2757c8c06d401
Mike Blakesley <m.e.blakesley@gmail.com>
2024-01-30 18:31:29 -0800

    Update grind75 #12 as review

--- grind75-12-linked-list-cycle.py
+++ grind75-12-linked-list-cycle.py
@@ -10,36 +10,13 @@ class ListNode:
 
 # https://leetcode.com/problems/linked-list-cycle/
 class Solution:
-    # speed is O(n), memory is O(n)
+    # O(n) time, O(1) space using classic hare & tortoise strategy
+    # 80th percentile
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-        node: ListNode = head
-        nodes_seen: set = set()
-        while node.next:
-            if node in nodes_seen:
+        tortoise, hair = head, head
+        while hair and hair.next:
+            tortoise = tortoise.next
+            hair = hair.next.next
+            if tortoise == hair:
                 return True
-            nodes_seen.add(node)
-            node = node.next
         return False
-
-    # example of memory-efficient version:
-    # speed is O(n^2), but memory is only O(1)
-    # plus, we could speed this up using tortoise & hare strategy
-    def hasCycle2(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-        curr_node: ListNode = head
-        i = 0
-        while True:
-            # i = 0 -> need to look 1 ahead. i = 1 -> need to look 2 ahead. etc
-            future_node: ListNode = curr_node
-            for _ in range(i + 1):
-                next_node: Optional[ListNode] = future_node.next
-                if not next_node:
-                    return False
-                if next_node == curr_node:
-                    return True
-                future_node = next_node
-            curr_node = curr_node.next
-            i += 1
