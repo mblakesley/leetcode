36c953ea23d79fcc6c3fe9dcf233251aa7006ef7
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-01 22:58:04 -0800

    Review grind75 #4: valid palindrome

--- grind75-05-valid-palindrome.py
+++ grind75-05-valid-palindrome.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/valid-palindrome
 class Solution:
-    # O(n)!
+    # O(n) time
     def isPalindrome(self, s: str) -> bool:
-        # yay comprehensions, but also, string immutability means looped '+=' is inefficient
-        s_plain: str = ''.join([char.lower() for char in s if char.isalnum()])
+        s_plain = ''.join(filter(str.isalnum, s)).lower()
         return s_plain == s_plain[::-1]
