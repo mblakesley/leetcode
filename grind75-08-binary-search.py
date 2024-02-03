eac2c1fc3efa2a9f4b7abcd35e3314f0ee8af7ba
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-03 10:47:30 -0800

    Review grind75 #8: binary search

--- grind75-08-binary-search.py
+++ grind75-08-binary-search.py
@@ -1,16 +1,14 @@
 # https://leetcode.com/problems/binary-search
 class Solution:
+    # O(log n) time
     def search(self, nums: list[int], target: int) -> int:
-        # min & max are inclusive
-        i_min: int = 0
-        i_max: int = len(nums) - 1
-        while i_min <= i_max:
-            i_half: int = (i_max + i_min) // 2
-            num: int = nums[i_half]
-            if target == num:
-                return i_half
-            elif target < num:
-                i_max = i_half - 1
+        min_, max_ = 0, len(nums) - 1
+        while min_ <= max_:
+            mid = (min_ + max_) // 2
+            if target == nums[mid]:
+                return mid
+            if target < nums[mid]:
+                max_ = mid - 1
             else:
-                i_min = i_half + 1
+                min_ = mid + 1
         return -1
