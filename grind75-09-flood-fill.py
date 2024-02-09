d65fd3227cf4043c8851b4cdc2e95e9260f08a30
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-08 19:22:31 -0800

    Review grind75 #9: flood fill

--- grind75-09-flood-fill.py
+++ grind75-09-flood-fill.py
@@ -1,27 +1,19 @@
 # https://leetcode.com/problems/flood-fill
 class Solution:
     def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
-        scolor: int = image[sr][sc]
-        tcolor: int = color
-        # don't kick off filling unnecessarily
-        if color == scolor:
+        target = image[sr][sc]
+        if target == color:
             return image
-
-        r_max: int = len(image)
-        c_max: int = len(image[0])
-
-        pixel_stack: list[tuple[int, int]] = [(sr, sc)]
-        while pixel_stack:
-            r, c = pixel_stack.pop()
-            if image[r][c] != scolor:
-                continue
-            image[r][c] = tcolor
-            if r-1 >= 0:
-                pixel_stack += [(r-1, c)]
-            if r+1 < r_max:
-                pixel_stack += [(r+1, c)]
-            if c-1 >= 0:
-                pixel_stack += [(r, c-1)]
-            if c+1 < c_max:
-                pixel_stack += [(r, c+1)]
+        r_max, c_max = len(image) - 1, len(image[0]) - 1
+        to_check = [(sr, sc)]
+        checked = {(sr, sc)}  # seems like this speeds it up a bit
+        while to_check:
+            r, c = to_check.pop()
+            if 0 <= r <= r_max and 0 <= c <= c_max and image[r][c] == target:
+                image[r][c] = color
+                for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                    r2, c2 = r + dr, c + dc
+                    if (r2, c2) not in checked:
+                        to_check.append((r2, c2))
+                        checked.add((r2, c2))
         return image
