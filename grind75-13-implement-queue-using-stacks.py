29ff0d46d48e8c8a7f16a11852ef7a6c9a50ebc1
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-10 15:08:03 -0800

    Review grind75 #13: implement queue using stacks

--- grind75-13-implement-queue-using-stacks.py
+++ grind75-13-implement-queue-using-stacks.py
@@ -1,52 +1,48 @@
 # https://leetcode.com/problems/implement-queue-using-stacks/
 # This Stack class is only described in the problem, but I explicitly wrote it out, just so we're all clear
 class Stack:
-    _stack: list[int]
+    stack: list[int]
 
     def __init__(self):
-        self._stack = []
+        self.stack = []
 
     def push(self, x: int) -> None:
-        self._stack.append(x)
+        self.stack.append(x)
 
     def peek(self) -> int:
-        return self._stack[-1]
+        return self.stack[-1]
 
     def pop(self) -> int:
-        return self._stack.pop()
+        return self.stack.pop()
 
     def size(self) -> int:
-        return len(self._stack)
+        return len(self.stack)
 
-    def empty(self) -> bool:
-        return not self._stack
+    def is_empty(self) -> bool:
+        return not self.stack
 
 
 class MyQueue:
-    _stack: Stack
-    _rev_stack: Stack
-
     def __init__(self):
-        self._stack = Stack()
-        self._rev_stack = Stack()
+        self.push_stack = Stack()
+        self.pop_stack = Stack()
 
     def push(self, x: int) -> None:
-        """append"""
-        self._stack.push(x)
+        self.push_stack.push(x)
 
     def pop(self) -> int:
-        """from front"""
-        self._flip_stacks()
-        return self._rev_stack.pop()
+        self._flip_if_needed()
+        return self.pop_stack.pop()
 
     def peek(self) -> int:
-        self._flip_stacks()
-        return self._rev_stack.peek()
-
-    def _flip_stacks(self) -> None:
-        if self._rev_stack.empty():
-            while not self._stack.empty():
-                self._rev_stack.push(self._stack.pop())
+        self._flip_if_needed()
+        return self.pop_stack.peek()
 
     def empty(self) -> bool:
-        return self._stack.empty() and self._rev_stack.empty()
+        return self.push_stack.is_empty() and self.pop_stack.is_empty()
+
+    # This is the trick! Only flip one way, and only when needed.
+    def _flip_if_needed(self):
+        if self.pop_stack.is_empty():
+            while not self.push_stack.is_empty():
+                self.pop_stack.push(self.push_stack.pop())
