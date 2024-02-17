4e968dc698d2d543ceff8a3fe72764da9b7ab656
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-17 10:58:53 -0800

    Review grind75 #26: insert interval

--- grind75-26-insert-interval.py
+++ grind75-26-insert-interval.py
@@ -1,16 +1,17 @@
 # https://leetcode.com/problems/insert-interval/
 class Solution:
-    # solution is 1*n
+    # O(n) solution - 70th percentile
     def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-        left, right = [], []
-        overlap_start, overlap_end = newInterval
-        for interval in intervals:
-            curr_start, curr_end = interval
-            if curr_end < overlap_start:
-                left.append(interval)
-            elif overlap_end < curr_start:
-                right.append(interval)
+        j, k = 0, len(intervals)
+        new_start, new_end = newInterval
+        for i, (start, end) in enumerate(intervals):
+            if end < new_start:
+                j += 1
+            elif start <= new_end:
+                new_start = min(new_start, start)
+                new_end = max(new_end, end)
             else:
-                overlap_start = min(overlap_start, curr_start)
-                overlap_end = max(overlap_end, curr_end)
-        return left + [overlap_start, overlap_end] + right
+                k = i  # we actually want i-1 but python's end boundary is exclusive
+                break
+        intervals[j:k] = [[new_start, new_end]]
+        return intervals
