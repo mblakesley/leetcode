de661e1b8055cf62f92396f917feed5769de895e
Mike Blakesley <m.e.blakesley@gmail.com>
2024-02-25 10:30:48 -0800

    Review grind75 #40: number of islands

--- grind75-40-number-of-islands.py
+++ grind75-40-number-of-islands.py
@@ -1,23 +1,21 @@
 # https://leetcode.com/problems/number-of-islands/
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        # find all land tiles
-        all_lands: set[tuple] = set()
-        for x, row in enumerate(grid):
-            for y, cell in enumerate(row):
-                if cell == "1":
-                    all_lands.add((x, y))
+        land_tiles = set()  # set of (r, c) tuples
+        for r, row in enumerate(grid):
+            for c, cell in enumerate(row):
+                if cell == '1':
+                    land_tiles.add((r, c))
 
-        isles = 0
-        while all_lands:
-            adjacent_lands = [all_lands.pop()]
-            isles += 1
-            # find all adjacent land tiles & remove them from the pool
-            while adjacent_lands:
-                x, y = adjacent_lands.pop()
-                neighbors = [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]
-                for neighbor in neighbors:
-                    if neighbor in all_lands:
-                        all_lands.remove(neighbor)
-                        adjacent_lands.append(neighbor)
-        return isles
+        island_count = 0
+        while land_tiles:
+            island_count += 1
+            curr_island = [land_tiles.pop()]
+            while curr_island:
+                r, c = curr_island.pop()
+                for neighbor in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
+                    if neighbor in land_tiles:
+                        land_tiles.discard(neighbor)
+                        curr_island.append(neighbor)
+
+        return island_count
