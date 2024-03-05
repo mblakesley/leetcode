3f1fb20d07425c5a6d11f0da2df7bc75ac070120
Mike Blakesley <m.e.blakesley@gmail.com>
2024-03-04 19:43:12 -0800

    Review grind75 #35: implement trie prefix tree

--- grind75-35-implement-trie-prefix-tree.py
+++ grind75-35-implement-trie-prefix-tree.py
@@ -1,33 +1,22 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/
 class Trie:
-    # Yeah, I could've/should've done it with Node objects, but I kept it a bit simpler for this scenario
     def __init__(self):
-        self.trie = {}
+        self.root = {}
+        self.delimiter = '.'
 
     def insert(self, word: str) -> None:
-        branch = self.trie
+        word += self.delimiter
+        parent = self.root
         for char in word:
-            branch = branch.setdefault(char, {})
-        branch['.'] = True
+            parent = parent.setdefault(char, {})
 
     def search(self, word: str) -> bool:
-        return self._search(word)
+        return self.startsWith(word + self.delimiter)
 
     def startsWith(self, prefix: str) -> bool:
-        return self._search(prefix, terminal=False)
-
-    def _search(self, string: str, terminal: bool = True) -> bool:
-        branch = self.trie
-        for char in string:
-            branch = branch.get(char)
-            if branch is None:
+        parent = self.root
+        for char in prefix:
+            parent = parent.get(char)
+            if parent is None:
                 return False
-        if terminal:
-            return branch.get('.', False)
         return True
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
