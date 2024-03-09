bfee08136f6b54b907886cc57dbe6f647169ddc2
Mike Blakesley <m.e.blakesley@gmail.com>
2024-03-09 08:37:50 -0800

    Review grind75 #41: rotting oranges

--- grind75-41-rotting-oranges.py
+++ grind75-41-rotting-oranges.py
@@ -1,24 +1,22 @@
 # https://leetcode.com/problems/rotting-oranges/
 class Solution:
     def orangesRotting(self, grid: list[list[int]]) -> int:
-        minute = 0
-        fresh: set[tuple] = set()  # set of (x, y)
-        rotten_queue: list[tuple] = []  # list of (x, y, minute rotten)
-        for x, row in enumerate(grid):
-            for y, cell in enumerate(row):
+        fresh = set()
+        newly_rotten = set()
+        for r, row in enumerate(grid):
+            for c, cell in enumerate(row):
                 if cell == 1:
-                    fresh.add((x, y))
+                    fresh.add((r, c))
                 elif cell == 2:
-                    rotten_queue += [(x, y, minute)]
+                    newly_rotten.add((r, c))
 
-        if not fresh:
-            return minute
-
-        while rotten_queue:  # BFS
-            x, y, minute = rotten_queue.pop(0)
-            neighbors: set[tuple] = {(x+1, y), (x, y+1), (x-1, y), (x, y-1)}
-            neighbors &= fresh  # we only care about fresh neighbors
-            fresh -= neighbors
-            rotten_queue += [(*neighbor, minute + 1) for neighbor in neighbors]
+        minute = 0
+        while fresh and newly_rotten:
+            rot_neighbors = set()
+            for r, c in newly_rotten:
+                rot_neighbors |= {(r, c+1), (r, c-1), (r+1, c), (r-1, c)}
+            newly_rotten = fresh & rot_neighbors
+            fresh -= newly_rotten
+            minute += 1
 
         return minute if not fresh else -1
