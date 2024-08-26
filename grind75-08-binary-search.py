50fbf7e38bff9df5d671e40b65b36216af7a2692
Mike Blakesley <m.e.blakesley@gmail.com>
2024-08-25 17:31:33 -0700

    Review grind75 #8: binary search

--- grind75-08-binary-search.py
+++ grind75-08-binary-search.py
@@ -2,13 +2,14 @@
 class Solution:
     # O(log n) time
     def search(self, nums: list[int], target: int) -> int:
-        min_, max_ = 0, len(nums) - 1
-        while min_ <= max_:
-            mid = (min_ + max_) // 2
-            if target == nums[mid]:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            num = nums[mid]
+            if num == target:
                 return mid
-            if target < nums[mid]:
-                max_ = mid - 1
+            if num < target:
+                low = mid + 1
             else:
-                min_ = mid + 1
+                high = mid - 1
         return -1
