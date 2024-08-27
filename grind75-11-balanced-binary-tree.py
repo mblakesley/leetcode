2353e790c543a876244cbad693d6fa2a848dd62e
Mike Blakesley <m.e.blakesley@gmail.com>
2024-08-27 15:17:17 -0700

    Review Grind75 #11: balanced binary tree

--- grind75-11-balanced-binary-tree.py
+++ grind75-11-balanced-binary-tree.py
@@ -13,13 +13,14 @@ class TreeNode:
 class Solution:
     # 90th percentile
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return self.bal_depth(root)[0]
+        _, is_bal = self.height_balance_check(root)
+        return is_bal
 
-    def bal_depth(self, node: Optional[TreeNode]) -> tuple[bool, int]:
-        if not node:
-            return True, 0
-        l_bal, l_depth = self.bal_depth(node.left)
-        r_bal, r_depth = self.bal_depth(node.right)
-        if not l_bal or not r_bal:  # minor short-circuit
-            return False, 0
-        return abs(l_depth - r_depth) <= 1, max(l_depth, r_depth) + 1
+    def height_balance_check(self, root: Optional[TreeNode]) -> tuple[int, bool]:
+        # returns (max_height, is_balanced)
+        if not root:
+            return (0, True)
+        lh, l_isbal = self.height_balance_check(root.left)
+        rh, r_isbal = self.height_balance_check(root.right)
+        # a node is only balanced if its children are balanced and it itself is balanced
+        return max(lh, rh) + 1, l_isbal and r_isbal and abs(lh - rh) <= 1
