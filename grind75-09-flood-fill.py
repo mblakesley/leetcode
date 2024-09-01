40f95d4cc586e1d684d6a502c4c30df01d70e4ba
Mike Blakesley <m.e.blakesley@gmail.com>
2024-09-01 11:25:40 -0700

    Review Grind75 #9: flood fill

--- grind75-09-flood-fill.py
+++ grind75-09-flood-fill.py
@@ -1,19 +1,19 @@
 # https://leetcode.com/problems/flood-fill
 class Solution:
+    # I didn't use a "seen" set for this one b/c I don't think it's much faster.
+    # The per-pixel logic is quite simple and the dupes are maxed at 4/pixel.
     def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
-        target = image[sr][sc]
-        if target == color:
-            return image
-        r_max, c_max = len(image) - 1, len(image[0]) - 1
-        to_check = [(sr, sc)]
-        checked = {(sr, sc)}  # seems like this speeds it up a bit
-        while to_check:
-            r, c = to_check.pop()
-            if 0 <= r <= r_max and 0 <= c <= c_max and image[r][c] == target:
-                image[r][c] = color
-                for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                    r2, c2 = r + dr, c + dc
-                    if (r2, c2) not in checked:
-                        to_check.append((r2, c2))
-                        checked.add((r2, c2))
+        src_color = image[sr][sc]
+        dest_color = color
+        r_max = len(image)-1
+        c_max = len(image[0])-1
+        to_fill = [(sr, sc)] if src_color != dest_color else []
+
+        while to_fill:
+            r, c = to_fill.pop()
+            if 0 <= r <= r_max and 0 <= c <= c_max and image[r][c] == src_color:
+                image[r][c] = dest_color
+                to_fill.extend(
+                    [(nr, nc) for (nr, nc) in ((r+1, c), (r-1, c), (r, c+1), (r, c-1))]
+                )
         return image
