cd2da53b7a2431ed1571fda26760a24e4dda8e8d
Mike Blakesley <m.e.blakesley@gmail.com>
2026-06-18 18:21:19 -0700

    Review Grind75 #12 and #18 (again)

--- grind75-12-linked-list-cycle.py
+++ grind75-12-linked-list-cycle.py
@@ -1,22 +1,20 @@
 from typing import Optional
 
-
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x: int):
         self.val: int = x
         self.next: Optional[ListNode] = None
 
-
 # https://leetcode.com/problems/linked-list-cycle/
 class Solution:
-    # O(n) time, O(1) space using classic two pointer (tortoise & hare) strategy
-    # 80th percentile
+    # Time complexity: O(n)
+    # Space complexity: O(1)
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        tortoise, hair = head, head
-        while hair and hair.next:
+        tortoise, hare = head, head
+        while hare and hare.next:
             tortoise = tortoise.next
-            hair = hair.next.next
-            if tortoise == hair:
+            hare = hare.next.next
+            if tortoise == hare:
                 return True
         return False
--- grind75-18-reverse-linked-list.py
+++ grind75-18-reverse-linked-list.py
@@ -10,11 +10,18 @@ class ListNode:
 
 # https://leetcode.com/problems/reverse-linked-list/
 class Solution:
+    # Iterative. Simple & clean.
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
         while curr:
-            # Can be done in one line, but IMO modifying curr AND curr.next simultaneously is asking for trouble
-            next_ = curr.next
-            curr.next = prev
-            prev, curr = curr, next_
+            prev, curr.next, curr = curr, prev, curr.next  # A bit confusing to read yet also clear
         return prev
+
+    # Recursive. The rare scenario where recursive isn't as nice.
+    def reverseListRecursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        tail = self.reverseListRecursive(head.next)
+        head.next.next = head
+        head.next = None
+        return tail
