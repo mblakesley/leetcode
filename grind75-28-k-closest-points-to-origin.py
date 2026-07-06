81f4331ef60d3419b0327c1b87fcff0cf171046c
Mike Blakesley <m.e.blakesley@gmail.com>
2026-07-06 15:58:49 -0700

    Review Grind75 #28: k closest points to origin (again)

--- grind75-28-k-closest-points-to-origin.py
+++ grind75-28-k-closest-points-to-origin.py
@@ -1,23 +1,26 @@
 import heapq
 
-
 # https://leetcode.com/problems/k-closest-points-to-origin/
 class Solution:
-    # O(n log k) using heap, 1-liner
+    # Ridiculous python 1-liner. Note that this is the only heapq function that accepts a sort key.
+    # Time complexity: O(n log k)
+    # Space complexity: O(k)
     def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
         return heapq.nsmallest(k, points, lambda xy: xy[0]**2 + xy[1]**2)
 
-    # O(n log k) using heap, done more manually to learn about heaps
-    def kClosest_heap_long(self, points: list[list[int]], k: int) -> list[list[int]]:
-        heap = [(-x**2 - y**2, x, y) for x, y in points[:k]]
-        heapq.heapify(heap)
-        for x, y in points[k:]:
-            sum_ = -x**2 - y**2
-            if sum_ > heap[0][0]:
-                heapq.heapreplace(heap, (sum_, x, y))
-        return [[x, y] for _, x, y in heap]
+    # Using normal heap functionality without nsmallest()
+    # Time complexity: O(n log k)
+    # Space complexity: O(k)
+    def k_closest_without_nsmallest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        max_heap = [(x**2 + y**2, x, y) for x, y in points[:k]]  # (z^2, x, y)
+        heapq.heapify_max(max_heap)
+        for i in range(k, len(points)):
+            x, y = points[i]
+            heapq.heappushpop_max(max_heap, (x**2 + y**2, x, y))  # pushpop() is more efficient than alternatives
+        return [[x, y] for _, x, y in max_heap]
 
-    # O(n log n) Mega naive 1-liner that's surprisingly performant, at least on leetcode's dataset
-    # As in, it's MUCH more performant than writing out a naive implementation of a heap.
-    def kClosest_kn(self, points: list[list[int]], k: int) -> list[list[int]]:
+    # Naive 1-liner. It's surprisingly performant compared to naive implementations of a heap!
+    # Time complexity: O(n log n)
+    # Space complexity: O(n) / O(log n) depending on sort implementation
+    def k_closest_without_heap(self, points: list[list[int]], k: int) -> list[list[int]]:
         return sorted(points, key=lambda xy: xy[0]**2 + xy[1]**2)[:k]
