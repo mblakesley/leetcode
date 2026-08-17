493f0d45ec50222db6564e60b614b4097d2577a9
Mike Blakesley <m.e.blakesley@gmail.com>
2026-08-17 01:43:48 -0700

    Review Grind75 #29: Longest substring without repeating chars

--- grind75-29-longest-substring-without-repeating-chars.py
+++ grind75-29-longest-substring-without-repeating-chars.py
@@ -1,30 +1,23 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class Solution:
-    # O(n) solution using queue + set (which could easily be modified to return the substring itself)
-    # There's a 2x faster solution using only a dict of {char: index} but it loses track of the actual substring
+    # Sliding window + set
+    # Time complexity: O(n)
+    # Aux space complexity: O(n)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        from collections import deque  # Only 'cuz python lacks a built-in queue
-        char_queue = deque()
-        uniques = set()
-        max_len = 0
-        for char in s:
-            if char in uniques:
-                max_len = max(max_len, len(uniques))
-                while char_queue[0] != char:
-                    uniques.discard(char_queue.popleft())
-                char_queue.popleft()
-            char_queue.append(char)
-            uniques.add(char)
-        return max(max_len, len(uniques))
+        l = 0
+        uniq_chars = set()
+        max_uniq = 0
+        for r, r_char in enumerate(s):
+            if r_char not in uniq_chars:
+                uniq_chars.add(r_char)
+                max_uniq = max(max_uniq, len(uniq_chars))
+            else:
+                l_char = s[l]
+                while l_char != r_char:
+                    uniq_chars.remove(s[l])
+                    l += 1
+                    l_char = s[l]
+                l += 1  # Advance L but leave the char in the set
+        return max_uniq
 
-    # Cuter but less performant
-    # O(k*n) where k can vary but should be proportional to the number of unique chars
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        substr = ''
-        max_len = 0
-        for char in s:
-            if char in substr:
-                max_len = max(max_len, len(substr))
-                substr = substr.split(char)[-1]
-            substr += char
-        return max(max_len, len(substr))
+    # TODO: There's a solution using only a dict of {char: index}. It's slightly more performant but more abstract.
